e8077cc52d3a944ac032d6c979ddbdad26888610
Ryan Day <soldair@gmail.com>
2015-11-11 06:11:40 -0800

    works!

--- index.js
+++ index.js
@@ -9,26 +9,40 @@ module.exports = function(patterns){
   if(!Array.isArray(patterns)) patterns = [patterns]
   
   var todo = patterns.length;
-  var streamFiles = []
-  
+  var streamFiles = []  
 
   deglob(patterns,function(err,files){
-    console.log(files) 
+    files.sort()
+    s.emit('files',files)
+    console.log(files)
+    stream(files)
   })
 
+  function stream (files){
+    var f = files.shift()
+    if(!f) return;
+    var rs = fs.createReadStream(f)
+    eos(rs,function(err){
+      if(err) {
+        err.file = f
+        return s.emit('error',err)
+      }
+      stream(files)
+    })
 
-  function stream(){
-    
+    var opts = {};
+    if(files.length) opts.end = false
+
+    rs.pipe(s,opts)
   }
-  
 
   return s;  
 }
 
 function deglob(patterns,cb,_res){
-  if(!patterns.length) cb(false,[])
-
+  if(!patterns.length) return cb(false,_res||[])
   if(!_res) _res = []
+
   glob(patterns.shift(),function(err,files){
     if(err) return cb(err)
     // filter for only files.
@@ -56,10 +70,12 @@ function filterFiles(files,cb){
 
   function work(){
     if(!files.length) return
+    
     var f = files.shift()
     fs.stat(f,function(err,stat){
       if(err) return cb(err)
       if(stat.isFile()) filtered.push(f)
+      if(!--todo) cb(false,filtered)
       work()
     })
   }
--- test/test.js
+++ test/test.js
@@ -1,5 +1,33 @@
 var test = require('tape')
 var readmany = require('../')
+var eos = require('end-of-stream')
+var fs = require('fs')
+
 test("can",function(t){
-  
+
+  t.plan(3)
+
+  var expected = Buffer.concat([
+      fs.readFileSync(__dirname+'/fixtures/a.log'),
+      fs.readFileSync(__dirname+'/fixtures/b.log'),
+      fs.readFileSync(__dirname+'/fixtures/c.log'),
+  ])+''
+
+  var bufs = []
+
+  var s = readmany(__dirname+'/fixtures/*.log')
+
+  s.on('files',function(files){
+    t.equals(files.length,3)
+  })
+
+  s.on('data',function(data){
+    bufs.push(data)
+  })
+
+  eos(s,function(err){
+    t.ok(!err,'should not have error!')
+    t.equals(Buffer.concat(bufs)+'',expected,'should have read all files in correct order')
+  })
+
 })
